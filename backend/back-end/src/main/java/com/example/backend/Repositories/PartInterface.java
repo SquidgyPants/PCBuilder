c4a3a15d15648086e82460144eab71936c0c180c
SquidgyPants <115804504+SquidgyPants@users.noreply.github.com>
2025-04-24 14:19:47 +0200

    Dockerfile addition

--- backend/back-end/src/main/java/com/example/backend/Repositories/PartInterface.java
+++ backend/back-end/src/main/java/com/example/backend/Repositories/PartInterface.java
@@ -1,6 +1,7 @@
 package com.example.backend.Repositories;
 
 import com.example.backend.DTOs.PartDTO;
+import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
+@Table
 public interface PartInterface extends CrudRepository<PartDTO, UUID> {
 
 }
